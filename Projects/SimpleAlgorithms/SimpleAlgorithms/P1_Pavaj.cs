b55cd983323cc592152f32ec66edeacc1a40f3a0
Ana Maria Rus <arus@gfi.com>
2015-09-30 14:22:57 +0300

    Some changes to P1 and P6

--- Projects/SimpleAlgorithms/SimpleAlgorithms/P1_Pavaj.cs
+++ Projects/SimpleAlgorithms/SimpleAlgorithms/P1_Pavaj.cs
@@ -10,8 +10,8 @@ namespace SimpleAlgorithms
     {
         public static int GetStonesNumber(ushort m, ushort n, ushort a)
         {
-            var columns = (m % a > 0) ? ((m / a) + 1) : (m / a);
-            var lines = (n % a > 0) ? ((n / a) + 1) : (n / a);
+            var columns = (m % a > 0) ? (m / a) + 1 : m / a;
+            var lines = (n % a > 0) ? (n / a) + 1 : n / a;
             return columns * lines;
         }
     }
--- Projects/SimpleAlgorithms/SimpleAlgorithms/P6_Sportiv.cs
+++ Projects/SimpleAlgorithms/SimpleAlgorithms/P6_Sportiv.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace SimpleAlgorithms
 {
     public static class P6_Sportiv
-    {
+    {        
         public static int GetWorkingRepetitions(int rounds)
         {
-            return rounds * rounds;
+            return (rounds == 0) ? rounds : (GetWorkingRepetitions(rounds -1) + 2*rounds -1);
         }
     }
 }
