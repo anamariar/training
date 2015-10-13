1b4b6b76aa3efa73763d6a200a47b7cc1a35c602
Ana Maria Rus <arus@gfi.com>
2015-10-13 14:20:11 +0300

    Add Reverse string and Replace char in string

---
+++ Projects/Algorithms/Algorithms/RecursiveProblems.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AlgorithmsTests")]
+namespace Algorithms
+{
+    public static class RecursiveProblems
+    {
+        internal static string ReverseString(string text)
+        {
+            if (text.Length == 1) return text;
+            return text[text.Length - 1] + ReverseString(text.Remove(text.Length - 1, 1));
+        }
+        internal static string ReplaceStringChar(string text, char originalChar, char replaceChar)
+        {
+            if (text.Length == 1) return ReplaceChar(text, originalChar, replaceChar);
+            return ReplaceChar(text[0].ToString(), originalChar, replaceChar) + ReplaceStringChar(text.Substring(1), originalChar, replaceChar);
+        }
+
+        private static string ReplaceChar(string text, char original, char replace)
+        {
+            if (text[0] == original) text = replace.ToString();
+            return text;
+        }
+    }
+}
---
+++ Projects/Algorithms/AlgorithmsTests/RecursiveProblemsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class RecursiveProblemsTests
+    {
+        [TestMethod]
+        public void ReverseStringTest()
+        {
+            string text = "abcd";
+            string expected = "dcba";
+            Assert.AreEqual(expected, RecursiveProblems.ReverseString(text));
+        }
+
+        [TestMethod]
+        public void ReverseOnCharStringTest()
+        {
+            string text = "a";
+            string expected = "a";
+            Assert.AreEqual(expected, RecursiveProblems.ReverseString(text));
+        }
+
+        [TestMethod]
+        public void ReplaceCharFormStringTest()
+        {
+            string text = "abacda";
+            char replaceChar = 'a';
+            char replaceWith = '*';
+            string expected = "*b*cd*";
+            Assert.AreEqual(expected, RecursiveProblems.ReplaceStringChar(text, replaceChar, replaceWith));
+        }
+
+    }
+}
