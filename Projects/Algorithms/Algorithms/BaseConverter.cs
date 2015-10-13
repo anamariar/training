0e42703ba471b8ebea245a41014a0d087e95c04a
Ana Maria Rus <arus@gfi.com>
2015-10-13 08:09:19 +0300

    Add BaseConverter project

---
+++ Projects/Algorithms/Algorithms/BaseConverter.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Algorithms
+{
+    public static class BaseConverter
+    {
+        public static byte[] ConvertIntoBase(ushort number, ushort intoBase)
+        {
+            byte[] result = new byte[Byte.MaxValue];
+            ushort size = 0;
+            if (number == 0) return new byte[] { 0 };
+            while (number > 0)
+            {
+                result[size] = (byte)(number % intoBase);
+                number /= intoBase;
+                size++;
+            }
+            Array.Resize(ref result, size);
+            Array.Reverse(result);
+            return result;
+        }
+
+        public static int ConvertFromBase(byte[] number, ushort fromBase)
+        {
+            int result = 0;
+            Array.Reverse(number);
+            for (ushort i= 0; i< number.Length; i++)
+            {
+                result += number[i] * GetPower(fromBase, i);
+            }
+            return result;
+        }
+
+        public static byte[] AndBaseTwo(byte[] firstNumber, byte[] secondNumber)
+        {            
+            return DoOperation(And, firstNumber, secondNumber);
+        }
+
+        public static byte[] OrBaseTwo(byte[] firstNumber, byte[] secondNumber)
+        {
+            return DoOperation(Or, firstNumber, secondNumber);
+        }
+
+        public static byte[] XorBaseTwo(byte[] firstNumber, byte[] secondNumber)
+        {
+            return DoOperation(Xor, firstNumber, secondNumber);
+        }
+
+        public static byte[] NotBaseTwo(byte[] number)
+        {
+            byte[] result = new byte[number.Length];
+            for (int i=0; i < number.Length; i++)
+            {
+                result[i] = Not(number[i]);
+            }
+            return result;
+        }
+
+        public static byte[] DoOperation(Func<byte, byte, byte> operation, byte[] firstNumber, byte[] secondNumber)
+        {
+            byte[] result = new byte[Math.Max(firstNumber.Length, secondNumber.Length)];
+            AddZerosToLeft(ref firstNumber, result.Length - firstNumber.Length);
+            AddZerosToLeft(ref secondNumber, result.Length - secondNumber.Length);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = operation(firstNumber[i], secondNumber[i]);
+            }
+            return result;
+        }        
+
+        public static byte[] Add(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        {
+            byte[] result = new byte[Math.Max(firstNumber.Length, secondNumber.Length)];
+            AddZerosToLeft(ref firstNumber, result.Length - firstNumber.Length);
+            AddZerosToLeft(ref secondNumber, result.Length - secondNumber.Length);
+            byte carry = 0;
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                result[i] = (byte)((firstNumber[i] + secondNumber[i] + carry) % numbersBase);
+                carry = (byte)((firstNumber[i] + secondNumber[i] + carry) / numbersBase);
+            }
+            result = AddCarryToLeft(result, carry);
+            return result;
+        }
+
+        private static byte[] AddCarryToLeft(byte[] result, byte carry)
+        {
+            if (carry == 1)
+            {
+                Array.Resize(ref result, result.Length + 1);
+                RotateRight(ref result, 1);
+                result[0] = carry;
+            }
+
+            return result;
+        }
+
+        public static byte[] Subtract(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        {
+            byte[] result = new byte[Math.Max(firstNumber.Length, secondNumber.Length)];
+            AddZerosToLeft(ref firstNumber, result.Length - firstNumber.Length);
+            AddZerosToLeft(ref secondNumber, result.Length - secondNumber.Length);
+            int carry = 0;
+            for (int i = result.Length - 1; i >= 0; i--)
+            {
+                if (carry + firstNumber[i] < secondNumber[i])
+                {
+                    result[i] = (byte)(numbersBase + firstNumber[i] + carry - secondNumber[i]);
+                    carry = -1;
+                }
+                else
+                {
+                    result[i] = (byte)(firstNumber[i] + carry - secondNumber[i]);
+                    carry = 0;
+                }
+            }
+            result = RemoveZerosFromLeft(result);
+            return result;
+        }
+
+        private static byte[] RemoveZerosFromLeft(byte[] result)
+        {
+            int positions = 0;
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                if (result[i] != 0) break;
+                else positions++;
+            }
+            RotateLeft(ref result, positions);
+            Array.Resize(ref result, result.Length - positions);
+            return result;
+        }
+
+        public static bool GreaterThan(byte[] greaterNumber, byte[] lowerNumber)
+        {
+            int length = 0;
+            length = Math.Max(greaterNumber.Length, lowerNumber.Length);
+            AddZerosToLeft(ref greaterNumber, length - greaterNumber.Length);
+            AddZerosToLeft(ref lowerNumber, length - lowerNumber.Length);
+            bool result = false;
+            for (int i = 0; i < length; i++)
+            {
+                if (greaterNumber[i] > lowerNumber[i])
+                {
+                    result = true;
+                    break;
+                }
+                else
+                {
+                    if (greaterNumber[i] < lowerNumber[i]) break;
+                }
+            }
+            return result;
+        }
+
+        public static byte[] Multiply(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        {
+            byte[] result = { 0 };
+            for (byte[] index = { 0 }; GreaterThan(secondNumber, index); index = Add(index, new byte[] { 1 }, numbersBase))
+            {
+                result = Add(result, firstNumber, numbersBase);
+            }
+            return result;
+        }
+
+        public static byte[] Divide(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        {
+            byte[] result = { 0 };
+            while (GreaterThan(firstNumber, Subtract(secondNumber, new byte[] { 1 }, numbersBase)))
+            {
+                firstNumber = Subtract(firstNumber, secondNumber, numbersBase);
+                result = Add(result, new byte[] { 1 }, numbersBase);
+            }
+            return result;
+        }
+
+        private static byte And(byte firstBit, byte secondBit)
+        {
+            return (firstBit != secondBit) ? (byte)0 : firstBit;
+        }
+
+        private static byte Or(byte firstBit, byte secondBit)
+        {
+            return (firstBit != secondBit) ? (byte)1 : firstBit;
+        }
+
+        private static byte Not(byte bit)
+        {
+            return (bit == 0) ? (byte)1 : (byte)0;
+        }
+
+        private static byte Xor(byte firstBit, byte secondBit)
+        {
+            return (firstBit != secondBit)? (byte)1 : (byte)0;
+        }
+
+        private static void RotateRight(ref byte[] number, int positions)
+        {            
+            for (int j = 1; j <= positions; j++)
+            {
+                byte[] rotatedNumber = new byte[number.Length];
+                for (int i = 0; i < number.Length - 1; i++)
+                {
+                    rotatedNumber[i + 1] = number[i];
+                }
+                rotatedNumber[0] = number[number.Length - 1];
+                number = rotatedNumber;
+            }        
+        }
+
+        private static void RotateLeft(ref byte[] number, int positions)
+        {
+            for (int j = 1; j <= positions; j++)
+            {
+                byte[] rotatedNumber = new byte[number.Length];
+                for (int i = 1; i < number.Length; i++)
+                {
+                    rotatedNumber[i - 1] = number[i];
+                }
+                rotatedNumber[number.Length - 1] = number[0];
+                number = rotatedNumber;
+            }
+        }
+
+        private static void AddZerosToLeft(ref byte[] number, int positions)
+        {
+            Array.Resize(ref number, number.Length + positions);
+            RotateRight(ref number, positions);
+        }
+
+        private static int GetPower(ushort number, ushort power)
+        {
+            return (power > 0) ? number * GetPower(number, (ushort)(power - 1)) : 1;
+        }
+    }
+}
---
+++ Projects/Algorithms/AlgorithmsTests/BaseConverterTests.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class BaseConverterTests
+    {
+        [TestMethod]
+        public void ConvertZeroInBaseTwoTest()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0 }, BaseConverter.ConvertIntoBase(0, 2));
+        }
+
+        [TestMethod]
+        public void ConvertNumberInBaseTwoTest()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 1, 0 }, BaseConverter.ConvertIntoBase(6, 2));
+        }
+
+        [TestMethod]
+        public void ConvertNumberInBaseEightTest()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 6 }, BaseConverter.ConvertIntoBase(70, 8));
+        }
+
+        [TestMethod]
+        public void ConvertNumberInBaseSixteenTest()
+        {
+            CollectionAssert.AreEqual(new byte[] { 3, 14, 8 }, BaseConverter.ConvertIntoBase(1000, 16));
+        }
+
+        [TestMethod]
+        public void ConvertZeroFromBaseTwoIntoBaseTenTest()
+        {
+            Assert.AreEqual(0, BaseConverter.ConvertFromBase(new byte[] { 0 }, 2));
+        }
+
+        [TestMethod]
+        public void ConvertNumberFromBaseTwoIntoBaseTenTest()
+        {
+            Assert.AreEqual(6, BaseConverter.ConvertFromBase(new byte[] { 1, 1, 0 }, 2));
+        }
+
+        [TestMethod]
+        public void ConvertNumberFromBaseEightIntoBaseTenTest()
+        {
+            Assert.AreEqual(70, BaseConverter.ConvertFromBase(new byte[] { 1, 0, 6 }, 8));
+        }
+
+        [TestMethod]
+        public void ConvertNumberFromBaseSixteenIntoBaseTenTest()
+        {
+            Assert.AreEqual(1000, BaseConverter.ConvertFromBase(new byte[] { 3, 14, 8 }, 16));
+        }
+
+        [TestMethod]
+        public void AndBetweenBaseTwoNumbers()
+        {
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1, 0, 0 };
+            byte[] expected = new byte[] { 1, 0, 0, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.AndBaseTwo(firstNumber, secondNumber));
+        }
+
+        [TestMethod]
+        public void AndBetweenBaseTwoNumbersOfDifferentLength()
+        {
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1 };
+            byte[] expected = new byte[] { 0, 0, 1, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.AndBaseTwo(firstNumber, secondNumber));
+        }
+
+        [TestMethod]
+        public void OrBetweenBaseTwoNumbers()
+        {
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1, 0, 0 };
+            byte[] expected = new byte[] { 1, 1, 1, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.OrBaseTwo(firstNumber, secondNumber));
+        }
+
+        [TestMethod]
+        public void OrBetweenBaseTwoNumbersOfDifferentLength()
+        {
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1 };
+            byte[] expected = new byte[] { 1, 0, 1, 1 };
+            CollectionAssert.AreEqual(expected, BaseConverter.OrBaseTwo(firstNumber, secondNumber));
+        }
+
+        [TestMethod]
+        public void XOrBetweenBaseTwoNumbers()
+        {
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1, 0, 0 };
+            byte[] expected = new byte[] { 0, 1, 1, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.XorBaseTwo(firstNumber, secondNumber));
+        }
+
+        [TestMethod]
+        public void XOrBetweenBaseTwoNumbersOfDifferentLength()
+        {
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1 };
+            byte[] expected = new byte[] { 1, 0, 0, 1 };
+            CollectionAssert.AreEqual(expected, BaseConverter.XorBaseTwo(firstNumber, secondNumber));
+        }
+
+        [TestMethod]
+        public void NotBaseTwoTest()
+        {
+            byte[] number = new byte[] { 1, 0, 1, 0 };
+            byte[] expected = new byte[] { 0, 1, 0, 1 };
+            CollectionAssert.AreEqual(expected, BaseConverter.NotBaseTwo(number));
+        }
+
+        [TestMethod]
+        public void AddNumbersInBaseTwoTest()
+        {
+            byte numbersBase = 2;
+            byte[] firstNumber = new byte[] { 1, 1, 0 };
+            byte[] secondNumber = new byte[] { 1 };
+            byte[] expected = new byte[] { 1, 1, 1 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Add(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void AddNumbersInBaseTwoWhenResultLengthIsHigherTest()
+        {
+            byte numbersBase = 2;            
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1, 1, 1, 0 };
+            byte[] expected = new byte[] { 1, 0, 1, 0, 0, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Add(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void AddNumbersInBaseEightTest()
+        {
+            byte numbersBase = 8;
+            byte[] firstNumber = new byte[] { 1, 0, 6 };
+            byte[] secondNumber = new byte[] { 1, 0, 6 };
+            byte[] expected = new byte[] { 2, 1, 4 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Add(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void AddNumbersBaseSixteenTest()
+        {
+            byte numbersBase = 16;
+            byte[] firstNumber = new byte[] { 3, 14, 8 };
+            byte[] secondNumber = new byte[] { 3, 14, 8 };
+            byte[] expected = new byte[] { 7, 13, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Add(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void AddNumbersInBaseSixteenWhenResultLengthIsHigher()
+        {
+            byte numbersBase = 16;
+            byte[] firstNumber = new byte[] { 12, 7 };
+            byte[] secondNumber = new byte[] { 12, 7 };
+            byte[] expected = new byte[] { 1, 8, 14 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Add(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void SubstractNumbersInBaseTwoTest()
+        {
+            byte numbersBase = 2;
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 1 };
+            byte[] expected = new byte[] { 1, 1, 1 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Subtract(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void SubstractNumbersInBaseEightTest()
+        {
+            byte numbersBase = 8;
+            byte[] firstNumber = new byte[] { 2, 1, 4 };
+            byte[] secondNumber = new byte[] { 1, 0, 6 };
+            byte[] expected = new byte[] { 1, 0, 6 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Subtract(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void SubstarctNumbersInBaseSixteenTest()
+        {
+            byte numbersBase = 16;
+            byte[] firstNumber = new byte[] { 1, 8, 14 };
+            byte[] secondNumber = new byte[] { 12, 7 };
+            byte[] expected = new byte[] { 12, 7 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Subtract(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void GreaterThanForBaseSixteenNumbersTest()
+        {
+            byte[] greaterNumber = new byte[] { 1, 8, 14 };
+            byte[] lowerNumber = new byte[] { 12, 7 };
+            Assert.IsTrue(BaseConverter.GreaterThan(greaterNumber, lowerNumber));
+        }
+
+        [TestMethod]
+        public void GreaterThanForBaseTwoNumbersOfDifferentLenghtsTest()
+        {
+            byte[] greaterNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] lowerNumber = new byte[] { 1, 1 };
+            Assert.IsFalse(BaseConverter.GreaterThan(lowerNumber, greaterNumber));
+        }
+
+        [TestMethod]
+        public void GreaterThanForBaseTwoNumbersTest()
+        {
+            byte[] greaterNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] lowerNumber = new byte[] { 1, 0, 0, 0 };
+            Assert.IsTrue(BaseConverter.GreaterThan(greaterNumber, lowerNumber));
+        }
+
+        [TestMethod]
+        public void MultiplyBaseTwoNumbersTest()
+        {
+            byte numbersBase = 2;
+            byte[] firstNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] secondNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] expected = new byte[] { 1, 1, 0, 0, 1, 0, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Multiply(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void MultiplyBaseEightNumbersTest()
+        {
+            byte numbersBase = 8;
+            byte[] firstNumber = new byte[] { 1, 2 };
+            byte[] secondNumber = new byte[] { 1, 5 };
+            byte[] expected = new byte[] { 2, 0, 2 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Multiply(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void MultiplyBaseSixteenNumbersTest()
+        {
+            byte numbersBase = 16;
+            byte[] firstNumber = new byte[] { 1, 14 };
+            byte[] secondNumber = new byte[] { 3, 2 };
+            byte[] expected = new byte[] { 5, 13, 12 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Multiply(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void DivideBaseTwoNumbersTest()
+        {
+            byte numbersBase = 2;
+            byte[] firstNumber = new byte[] { 1, 1, 0, 0, 1, 0, 0 };
+            byte[] secondNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] expected = new byte[] { 1, 0, 1, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Divide(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void DivideBaseTwoNumbersThatDontDivideExactlyTest()
+        {
+            byte numbersBase = 2;
+            byte[] firstNumber = new byte[] { 1, 1, 0, 0, 1, 0, 1 };
+            byte[] secondNumber = new byte[] { 1, 0, 1, 0 };
+            byte[] expected = new byte[] { 1, 0, 1, 0 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Divide(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void DivideBaseEightNumbersTest()
+        {
+            byte numbersBase = 8;
+            byte[] firstNumber = new byte[] { 2, 0, 2 };
+            byte[] secondNumber = new byte[] { 1, 5 };
+            byte[] expected = new byte[] { 1, 2 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Divide(firstNumber, secondNumber, numbersBase));
+        }
+
+        [TestMethod]
+        public void DivideBaseSixteenNumbersTest()
+        {
+            byte numbersBase = 16;
+            byte[] firstNumber = new byte[] { 5, 13, 12 };
+            byte[] secondNumber = new byte[] { 3, 2 };
+            byte[] expected = new byte[] { 1, 14 };
+            CollectionAssert.AreEqual(expected, BaseConverter.Divide(firstNumber, secondNumber, numbersBase));
+        }
+    }
+}
