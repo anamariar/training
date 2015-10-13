1c60d20dd8a53543825df71f323f2b85649ff8f9
Ana Maria Rus <arus@gfi.com>
2015-10-13 08:30:04 +0300

    Make methods internal and visible for tests

--- Projects/Algorithms/Algorithms/BaseConverter.cs
+++ Projects/Algorithms/Algorithms/BaseConverter.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("AlgorithmsTests")]
 namespace Algorithms
 {
     public static class BaseConverter
     {
-        public static byte[] ConvertIntoBase(ushort number, ushort intoBase)
+        internal static byte[] ConvertIntoBase(ushort number, ushort intoBase)
         {
             byte[] result = new byte[Byte.MaxValue];
             ushort size = 0;
@@ -25,33 +22,33 @@ namespace Algorithms
             return result;
         }
 
-        public static int ConvertFromBase(byte[] number, ushort fromBase)
+        internal static int ConvertFromBase(byte[] number, ushort fromBase)
         {
             int result = 0;
             Array.Reverse(number);
             for (ushort i= 0; i< number.Length; i++)
             {
-                result += number[i] * GetPower(fromBase, i);
+                result += number[i] * Power(fromBase, i);
             }
             return result;
         }
 
-        public static byte[] AndBaseTwo(byte[] firstNumber, byte[] secondNumber)
+        internal static byte[] AndBaseTwo(byte[] firstNumber, byte[] secondNumber)
         {            
             return DoOperation(And, firstNumber, secondNumber);
         }
 
-        public static byte[] OrBaseTwo(byte[] firstNumber, byte[] secondNumber)
+        internal static byte[] OrBaseTwo(byte[] firstNumber, byte[] secondNumber)
         {
             return DoOperation(Or, firstNumber, secondNumber);
         }
 
-        public static byte[] XorBaseTwo(byte[] firstNumber, byte[] secondNumber)
+        internal static byte[] XorBaseTwo(byte[] firstNumber, byte[] secondNumber)
         {
             return DoOperation(Xor, firstNumber, secondNumber);
         }
 
-        public static byte[] NotBaseTwo(byte[] number)
+        internal static byte[] NotBaseTwo(byte[] number)
         {
             byte[] result = new byte[number.Length];
             for (int i=0; i < number.Length; i++)
@@ -61,7 +58,7 @@ namespace Algorithms
             return result;
         }
 
-        public static byte[] DoOperation(Func<byte, byte, byte> operation, byte[] firstNumber, byte[] secondNumber)
+        internal static byte[] DoOperation(Func<byte, byte, byte> operation, byte[] firstNumber, byte[] secondNumber)
         {
             byte[] result = new byte[Math.Max(firstNumber.Length, secondNumber.Length)];
             AddZerosToLeft(ref firstNumber, result.Length - firstNumber.Length);
@@ -74,38 +71,28 @@ namespace Algorithms
             return result;
         }        
 
-        public static byte[] Add(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        internal static byte[] Add(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
         {
             byte[] result = new byte[Math.Max(firstNumber.Length, secondNumber.Length)];
             AddZerosToLeft(ref firstNumber, result.Length - firstNumber.Length);
             AddZerosToLeft(ref secondNumber, result.Length - secondNumber.Length);
+
             byte carry = 0;
             for (int i = result.Length - 1; i >= 0; i--)
             {
                 result[i] = (byte)((firstNumber[i] + secondNumber[i] + carry) % numbersBase);
                 carry = (byte)((firstNumber[i] + secondNumber[i] + carry) / numbersBase);
             }
-            result = AddCarryToLeft(result, carry);
+            AddCarryToLeft(ref result, carry);
             return result;
         }
 
-        private static byte[] AddCarryToLeft(byte[] result, byte carry)
-        {
-            if (carry == 1)
-            {
-                Array.Resize(ref result, result.Length + 1);
-                RotateRight(ref result, 1);
-                result[0] = carry;
-            }
-
-            return result;
-        }
-
-        public static byte[] Subtract(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        internal static byte[] Subtract(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
         {
             byte[] result = new byte[Math.Max(firstNumber.Length, secondNumber.Length)];
             AddZerosToLeft(ref firstNumber, result.Length - firstNumber.Length);
             AddZerosToLeft(ref secondNumber, result.Length - secondNumber.Length);
+
             int carry = 0;
             for (int i = result.Length - 1; i >= 0; i--)
             {
@@ -120,29 +107,17 @@ namespace Algorithms
                     carry = 0;
                 }
             }
-            result = RemoveZerosFromLeft(result);
+            RemoveZerosFromLeft(ref result);
             return result;
         }
 
-        private static byte[] RemoveZerosFromLeft(byte[] result)
-        {
-            int positions = 0;
-            for (int i = 0; i < result.Length - 1; i++)
-            {
-                if (result[i] != 0) break;
-                else positions++;
-            }
-            RotateLeft(ref result, positions);
-            Array.Resize(ref result, result.Length - positions);
-            return result;
-        }
-
-        public static bool GreaterThan(byte[] greaterNumber, byte[] lowerNumber)
+        internal static bool GreaterThan(byte[] greaterNumber, byte[] lowerNumber)
         {
             int length = 0;
             length = Math.Max(greaterNumber.Length, lowerNumber.Length);
             AddZerosToLeft(ref greaterNumber, length - greaterNumber.Length);
             AddZerosToLeft(ref lowerNumber, length - lowerNumber.Length);
+
             bool result = false;
             for (int i = 0; i < length; i++)
             {
@@ -159,7 +134,7 @@ namespace Algorithms
             return result;
         }
 
-        public static byte[] Multiply(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        internal static byte[] Multiply(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
         {
             byte[] result = { 0 };
             for (byte[] index = { 0 }; GreaterThan(secondNumber, index); index = Add(index, new byte[] { 1 }, numbersBase))
@@ -169,7 +144,7 @@ namespace Algorithms
             return result;
         }
 
-        public static byte[] Divide(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
+        internal static byte[] Divide(byte[] firstNumber, byte[] secondNumber, byte numbersBase)
         {
             byte[] result = { 0 };
             while (GreaterThan(firstNumber, Subtract(secondNumber, new byte[] { 1 }, numbersBase)))
@@ -179,7 +154,7 @@ namespace Algorithms
             }
             return result;
         }
-
+        
         private static byte And(byte firstBit, byte secondBit)
         {
             return (firstBit != secondBit) ? (byte)0 : firstBit;
@@ -228,15 +203,37 @@ namespace Algorithms
             }
         }
 
+        private static void AddCarryToLeft(ref byte[] result, byte carry)
+        {
+            if (carry == 1)
+            {
+                Array.Resize(ref result, result.Length + 1);
+                RotateRight(ref result, 1);
+                result[0] = carry;
+            }
+        }
+
+        private static void RemoveZerosFromLeft(ref byte[] result)
+        {
+            int positions = 0;
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                if (result[i] != 0) break;
+                else positions++;
+            }
+            RotateLeft(ref result, positions);
+            Array.Resize(ref result, result.Length - positions);
+        }
+
         private static void AddZerosToLeft(ref byte[] number, int positions)
         {
             Array.Resize(ref number, number.Length + positions);
             RotateRight(ref number, positions);
         }
 
-        private static int GetPower(ushort number, ushort power)
+        private static int Power(ushort number, ushort exponent)
         {
-            return (power > 0) ? number * GetPower(number, (ushort)(power - 1)) : 1;
+            return (exponent > 0) ? number * Power(number, (ushort)(exponent - 1)) : 1;
         }
     }
 }
