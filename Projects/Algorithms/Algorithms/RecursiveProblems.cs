bb4759707790a7883de3bd06b126a09f735fa58b
Ana Maria Rus <arus@gfi.com>
2015-10-14 14:40:06 +0300

    Add Calculator and Pascal problems

--- Projects/Algorithms/Algorithms/RecursiveProblems.cs
+++ Projects/Algorithms/Algorithms/RecursiveProblems.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("AlgorithmsTests")]
 namespace Algorithms
@@ -21,5 +23,53 @@ namespace Algorithms
             if (text[0] == original) text = replace.ToString();
             return text;
         }
+
+        internal static uint[] GetPascalTriangleRow(uint row)
+        {
+            uint[] result = new uint[row + 1];
+            uint[] previous = new uint[row];
+            result[0] = 1;
+            if (row > 0)
+            {
+                result[row] = 1;
+                previous = GetPascalTriangleRow(row - 1);
+            }
+            for (int i = 1; i < row; i++)
+            {               
+                result[i] = previous[i] + previous[i - 1];
+            }
+            return result;
+        }        
+
+        internal static float Calculator(string text)
+        {
+            if (text.Length < 3) throw new InvalidOperationException();
+
+            string[] textArray = text.Split(' ');            
+            if (textArray.Length == 3) return DoOperation(textArray[0], textArray[2], textArray[1]);
+
+            var firstElement = textArray[0];
+            var lastElement = textArray[textArray.Length - 1];
+            string[] newTextArray = new string[textArray.Length - 2];
+            Array.Copy(textArray, 1, newTextArray, 0, newTextArray.Length);
+            string newText = String.Join(" ", newTextArray);
+            return DoOperation(firstElement, Calculator(newText).ToString(CultureInfo.InvariantCulture), lastElement);
+        }
+
+        private static float DoOperation(string stringOperator, string firstOperand, string secondOperand)
+        {          
+            var firstNumber = float.Parse(firstOperand, CultureInfo.InvariantCulture.NumberFormat);
+            var secondNumber = float.Parse(secondOperand, CultureInfo.InvariantCulture.NumberFormat);
+            switch (stringOperator)
+            {
+                case "+": return firstNumber + secondNumber;
+                case "-": return firstNumber - secondNumber;
+                case "*": return firstNumber * secondNumber;
+                case "/": return firstNumber / secondNumber;
+                default: break;
+            }
+            return 0;
+        }
+
     }
 }
--- Projects/Algorithms/AlgorithmsTests/RecursiveProblemsTests.cs
+++ Projects/Algorithms/AlgorithmsTests/RecursiveProblemsTests.cs
@@ -33,5 +33,63 @@ namespace AlgorithmsTests
             Assert.AreEqual(expected, RecursiveProblems.ReplaceStringChar(text, replaceChar, replaceWith));
         }
 
+        [TestMethod]
+        public void VerifyFirstRowOfPascalTriangleTest()
+        {
+            uint[] expected = new uint[] { 1 };
+            CollectionAssert.AreEqual(expected, RecursiveProblems.GetPascalTriangleRow(0));
+        }
+
+        [TestMethod]
+        public void VerifySecondRowOfPascalTriangleTest()
+        {
+            uint[] expected = new uint[] { 1, 1 };
+            CollectionAssert.AreEqual(expected, RecursiveProblems.GetPascalTriangleRow(1));
+        }
+
+        [TestMethod]
+        public void VerifyRowOfPascalTriangleTest()
+        {
+            uint[] expected = new uint[] { 1, 6, 15, 20, 15, 6, 1 };
+            CollectionAssert.AreEqual(expected, RecursiveProblems.GetPascalTriangleRow(6));
+        }        
+        
+        [TestMethod]
+        public void CalculatorOneOperationTest()
+        {
+            string text = "* 3 4";
+            Assert.AreEqual(12f, RecursiveProblems.Calculator(text));
+        }
+
+        [TestMethod]
+        public void CalculatorTwoOperationsTest()
+        {
+            string text = "* + 1 1 2";
+            Assert.AreEqual(4f, RecursiveProblems.Calculator(text));
+        }
+
+        [TestMethod]
+        public void CalculatorMultipleOperationsTest()
+        {
+            string text = "* / * + 56 45 45 3 0.75";
+            Assert.AreEqual(1136.25f, RecursiveProblems.Calculator(text));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CalculatorInvalidStringTest()
+        {
+            string text = "* 3";
+            Assert.AreEqual(0f, RecursiveProblems.Calculator(text));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void CalculatorInvalidStringOperatorAtTheEndTest()
+        {
+            string text = "* 3 *";
+            Assert.AreEqual(0f, RecursiveProblems.Calculator(text));
+        }
+        
     }
 }
