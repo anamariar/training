d9b2d181b217f0daae67e054d75cf480a897d431
Ana Maria Rus <arus@gfi.com>
2015-10-19 14:18:51 +0300

    Sort Lottery numbers

---
+++ Projects/Algorithms/Algorithms/FindAndSortProblems.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class FindAndSortProblems
+    {
+        internal static uint[] SortLotteryNumbers(uint[] loterryNumbers)
+        {
+            uint[] sorted = new uint[0];
+            for (uint i=0; i< loterryNumbers.Length; i++)
+            {
+                InsertNumber(ref sorted, loterryNumbers[i]);
+            }
+            return sorted;
+        }
+
+        private static void InsertNumber(ref uint[] sorted, uint number)
+        {
+            Array.Resize(ref sorted, sorted.Length + 1);
+            int index = sorted.Length - 1;
+            for (int i = 0; i < sorted.Length - 1; i++)
+            {
+                if (number < sorted[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            for (int j = sorted.Length - 1; j > index; j--)
+            {
+                sorted[j] = sorted[j - 1];
+            }
+            sorted[index] = number;
+        }
+    }
+}
---
+++ Projects/Algorithms/AlgorithmsTests/FindAndSortProblemsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class FindAndSortProblemsTests
+    {
+        [TestMethod]
+        public void SortLotteryNumbersTest()
+        {
+            uint[] loterryNumbers = new uint[] { 33, 3, 49, 16, 17, 44};
+            uint[] expected = new uint[] { 3, 16, 17, 33, 44, 49 };
+            var sorted = FindAndSortProblems.SortLotteryNumbers(loterryNumbers);
+            CollectionAssert.AreEqual(expected, sorted);
+        }
+    }
+}
